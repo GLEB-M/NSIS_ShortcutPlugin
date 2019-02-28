8d352cd338d35ac4dc18246257e67c7e58542000
dx8vb <34866866+dx8vb@users.noreply.github.com>
2019-02-28 14:28:24 +0600

    update mingw build script

--- Shortcut/mingw_build.cmd
+++ Shortcut/mingw_build.cmd
@@ -4,6 +4,6 @@ REM Specify the path for MinGW compiler
 
 set PATH=C:\mingw-w64\i686-7.2.0-win32-dwarf-rt_v5-rev1\mingw32\bin
 
-g++ Shortcut.cpp -e_DllMain@12 -lkernel32 -luser32 -lOle32 -lUuid -lmingwex -nostdlib -shared -O3 -Wl,--kill-at -o Shortcut.dll
+g++ Shortcut.cpp -e_DllMain@12 -lkernel32 -luser32 -lOle32 -lUuid -lmingwex -nostdlib -shared -O2 -s -ffunction-sections -Wl,--kill-at,--gc-sections -o Shortcut.dll
 
 pause
